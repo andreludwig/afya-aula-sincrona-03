b9770766d5d03bb44db60b710dc12819bb5c2ceb
andreludwig <andre@roadseg.com.br>
2021-05-22 15:01:40 -0300

    Navbar include style

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,19 +2,22 @@ import React from 'react';
 
 import { Link } from 'react-router-dom'
 
+import { NavBarContent } from './style'
 import LogoAfya from '../../assets/img/logo.png'
 
 const NavBar: React.FC = () => {
   return (
-      <nav>
+      <NavBarContent>
           <Link to="/"> 
             <img src={LogoAfya} alt="Logo Afya" />
           </Link>
-          <Link to="/"> Home </Link>
-          <Link to="/criar-conta"> Inscreva-se </Link>
-          <Link to="/login"> Logar </Link>
+          <div className="links-content">
+            <Link to="/"> Home </Link>
+            <Link to="/criar-conta"> Inscreva-se </Link>
+            <Link to="/login"> Logar </Link>
+          </div>
 
-      </nav>
+      </NavBarContent>
   );
 }
 
